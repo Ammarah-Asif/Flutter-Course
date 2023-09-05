a75e5069b6a7d7479bd3fbde8584515d51b20967
Ammarah Asif <ammarahmukaty20@gmail.com>
2023-09-05 19:36:38 +0500

    Calculator

---
+++ calculator_task/lib/buttonvalues.dart
@@ -0,0 +1,44 @@
+class Btn{
+  static const String delete = "Del";
+   static const String clear = "Clear";
+   static const String percentage = "%";
+   static const String multiply = "*";
+   static const String divide = "/";
+   static const String add = "+";
+   static const String subtract = "-";
+   static const String calculate = "=";
+   static const String decimal = ".";
+
+   static const String v0 = "0";
+   static const String v1 = "1";
+   static const String v2 = "2";
+   static const String v3 = "3";
+   static const String v4 = "4";
+   static const String v5 = "5";
+   static const String v6 = "6";
+   static const String v7 = "7";
+   static const String v8 = "8";
+   static const String v9 = "9";
+
+   static const List<String> buttonssValues = [
+    delete,
+    clear,
+    percentage,
+    multiply,
+    v7,
+    v8,
+    v9,
+    divide,
+    v4,
+    v5,
+    v6,
+    subtract,
+    v1,
+    v2,
+    v3,
+    add,
+    v0,
+    decimal,
+    calculate,
+  ];
+}
